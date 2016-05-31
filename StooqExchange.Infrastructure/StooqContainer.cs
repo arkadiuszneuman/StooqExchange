385cd2a6c80bd5166eb500c3e03fd65309782940
Arkadiusz Neuman <aneuman@insolutions.pl>
2016-05-31 21:53:02 +0200

    Added container responsible for registering Autofac modules.

---
+++ StooqExchange.Infrastructure/StooqContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace StooqExchange.Infrastructure
+{
+    public class StooqContainer
+    {
+        public IContainer CreateContainer()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterAssemblyModules(GetType().GetTypeInfo().Assembly);
+
+            return builder.Build();
+        }
+    }
+}
