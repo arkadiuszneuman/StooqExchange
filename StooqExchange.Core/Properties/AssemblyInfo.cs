0682ce575eaacfa8d18b561a38d7481efc4f4a7d
Arkadiusz Neuman <aneuman@insolutions.pl>
2016-05-31 21:03:08 +0200

    Initialized solution structure.
    Added 2 project:
    - Infrastructure - stores information about Dependency Injection in Core.
    - Core - stores core classes responsible for getting information from stooq.pl.

---
+++ StooqExchange.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StooqExchange.Core")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("06bd337f-ca01-483d-9e71-1f69e7f0d596")]
